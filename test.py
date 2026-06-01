041fa5e987cd227251a7aedf3ec3dba9994e275d
BrandonWang269 <brandonw269@nycstudents.net>
2026-06-01 10:00:15 -0400

    dasd

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
 import requests
-
-response = requests.get("https://api.github.com")
-print(response.json())
+import tkinter as tk
+window = tk.Tk()
+window.title("Disney Character Search")
+window.geometry ("500x400")
+def search():
+    name = entry.get().lower()
+    link = "https://api.disneyapi.dev/character"
+    response = requests.get(link)
+    data = response.json()
+    found = False   
+    for disney in data["data"]:
+        if disney["name"].lower() == name:
+            result.config(text=f"Name: {disney['name']}\nFilms: {', '.join(disney['films'])}")
+            found = True
+            break
+        if not found:
+            result.config(text="Character not found.")
+titel_label = tk.Label(window, text="Search Disney Character")
+titel_label.pack(pady=20)
+entry = tk.Entry(window, width=30)
+entry.pack(pady=10)
+search_button = tk.Button(window, text="Search", command=search)
+search_button.pack(pady=10)
+result = tk.Label(window, text="", wraplength=450)
+result.pack(pady=20)
+window.mainloop()
